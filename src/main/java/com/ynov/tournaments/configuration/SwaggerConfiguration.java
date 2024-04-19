0c15ca5a789f7937172053e058c6f199227c5725
Delphine <delphine.pva@gmail.com>
2024-04-19 12:07:05 +0200

    swagger file updated, new endpoint added

--- src/main/java/com/ynov/tournaments/configuration/SwaggerConfiguration.java
+++ src/main/java/com/ynov/tournaments/configuration/SwaggerConfiguration.java
@@ -2,21 +2,29 @@ package com.ynov.tournaments.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 
 @Configuration
-public class SwaggerConfiguration {
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
-  @Bean
-  public OpenAPI openAPI() {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("API Web services")
+                .version("1.0")
+                .description("This API exposes endpoints to board games.");
 
-    Info info = new Info()
-        .title("API Web services")
-        .version("1.0")
-        .description("This API exposes endpoints to board games.");
+        return new OpenAPI().info(info);
+    }
 
-    return new OpenAPI().info(info);
-  }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
 }
