d989df374f43a3aa0c2b0d0073350164052512aa
Delphine <delphine.pva@gmail.com>
2024-04-26 15:49:41 +0200

    debug added

--- src/main/java/com/ynov/tournaments/TournamentsApplication.java
+++ src/main/java/com/ynov/tournaments/TournamentsApplication.java
@@ -2,12 +2,25 @@ package com.ynov.tournaments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class TournamentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TournamentsApplication.class, args);
+	}
+	
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(true);
+		loggingFilter.setMaxPayloadLength(64000);
+		return loggingFilter;
 	}
 
 }
