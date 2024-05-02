a3d8afb1a717f54dd17ce1dd73df1267f6f2e2b3
Llelepipede <pchesneau3103@gmail.com>
2024-05-02 11:24:15 +0200

    rabbit added

--- src/main/java/com/ynov/tournaments/TournamentsApplication.java
+++ src/main/java/com/ynov/tournaments/TournamentsApplication.java
@@ -4,11 +4,64 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class TournamentsApplication {
 
 	public static void main(String[] args) {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		factory.setPort(5672);
+		factory.setUsername("llele");
+		factory.setPassword("admin");
+		Connection connection = null;
+		try {
+			connection = factory.newConnection();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (TimeoutException e) {
+			throw new RuntimeException(e);
+		}
+		Channel channel = null;
+		try {
+			channel = connection.createChannel();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		try {
+			channel.exchangeDeclare("ok", "fanout");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		String queueName = "test";
+
+		try {
+			channel.queueBind(queueName, "", "");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			String message = new String(delivery.getBody(), "UTF-8");
+			System.out.println(" [x] Received '" + message + "'");
+		};
+		try {
+			channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
 		SpringApplication.run(TournamentsApplication.class, args);
 		System.out.print("Hello world");
 	}
